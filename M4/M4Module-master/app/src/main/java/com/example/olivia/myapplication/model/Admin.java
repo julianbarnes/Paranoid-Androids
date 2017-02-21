13bea3b7ccb095ea95b379398d7e0d98d5a2fa1a
otoaneba <naotoabe1@gmail.com>
2017-02-21 01:18:44 -0500

    made worker a subclass of user, manager a subclass of worker, then Admin a subclass of manager

--- M4/M4Module-master/app/src/main/java/com/example/olivia/myapplication/model/Admin.java
+++ M4/M4Module-master/app/src/main/java/com/example/olivia/myapplication/model/Admin.java
@@ -6,9 +6,29 @@ package com.example.olivia.myapplication.model;
  *  a sub class of user. takes in a name and a password
  */
 
-public class Admin extends User {
+public class Admin extends Manager {
 
     public Admin(String id, String name, String password, String email, String address, String userType) {
         super(id, name, password, email, address, userType);
+    }
+
+    public void deleteAccount() {
+        System.out.println("delete account");
+    }
+
+    public void banAccount() {
+        System.out.println("banned account");
+    }
+
+    public void lockAccount() {
+        System.out.println("locked account");
+    }
+
+    public void unlockAccount() {
+        System.out.println("unlocked account");
+    }
+
+    public void viewSecurityLog() {
+        System.out.println("viewing security log");
     }
 }
